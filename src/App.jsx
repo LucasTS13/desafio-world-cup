32dda8aa620eafb1485e5dba1604d4d2747b883d
LucasTS13 <lucast.santos2003@gmail.com>
2026-04-23 18:11:06 -0300

    feat: array da tabela pronto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Header from "./components/Header";
-import {shuffleTeams, generateGroups, generateMatches, simulateMatch,} from "./utils/tournament";
+import {shuffleTeams, generateGroups, generateMatches, simulateMatch, calculateStandings} from "./utils/tournament";
 
 function App() {
 const [groups, setGroups] = useState([]);
 
 const generateCup = async () => {
+    
+    //requisicao de times
     const res = await fetch(
       "https://development-internship-api.geopostenergy.com/WorldCup/GetAllTeams",
       {
@@ -14,13 +16,14 @@ const generateCup = async () => {
     );
     const data = await res.json();
     
-    const shuffled = shuffleTeams(data);
-    const groupsGenerated = generateGroups(shuffled);
+    const shuffled = shuffleTeams(data); //embaralha
+    const groupsGenerated = generateGroups(shuffled); //gera grupos
 
     const fullGroups = groupsGenerated.map(group => {
-      const matches = generateMatches(group);
-      const played = matches.map(simulateMatch);
-      return { teams: group, matches: played};
+      const matches = generateMatches(group); //gera partidas
+      const played = matches.map(simulateMatch); //simula resultado das partidas
+      const table = calculateStandings(group, played); //calcula tabela final
+      return { teams: group, matches: played, table};
     });
     console.log(fullGroups);
     
--- src/utils/tournament.js
+++ src/utils/tournament.js
@@ -1,28 +1,63 @@
+//recebe o arry de todos os times da competição e embaralha
 export function shuffleTeams(teams) {
-  return [...teams].sort(() => Math.random());
+    return [...teams].sort(() => Math.random());
 }
 
+//gera os grupos da competicao
 export function generateGroups(teams) {
-  const groups = [];
-  for (let i = 0; i < 8; i++) {
-    groups.push(teams.slice(i * 4, i * 4 + 4));
-  }
-  return groups;
-}
-
-export function generateMatches(group) {
-  const matches = [];
-  for (let i = 0; i < group.length; i++) {
-    for (let j = i + 1; j < group.length; j++) {
-      matches.push({ teamA: group[i].nome, teamB: group[j].nome });
+    const groups = [];
+    for (let i = 0; i < 8; i++) {
+        groups.push(teams.slice(i * 4, i * 4 + 4)); //i=0, vai fazer os slice começando em 0 e terminando em 4, porem nao inclui o indice 4
     }
-  }
-  return matches;
+    return groups;
 }
 
+//gera as partidas entre cada grupo
+export function generateMatches(group) {
+    const matches = [];
+    for (let i = 0; i < group.length; i++) {
+        for (let j = i + 1; j < group.length; j++) {
+        matches.push({ teamA: group[i].nome, teamB: group[j].nome });
+        }
+    }
+    return matches;
+}
+
+//adiciona o campo de gols as partidas
 export function simulateMatch(match) {
-  const goalsA = Math.floor(Math.random() * 6);
-  const goalsB = Math.floor(Math.random() * 6);
+    const goalsA = Math.floor(Math.random() * 6);
+    const goalsB = Math.floor(Math.random() * 6);
 
   return { ...match, goalsA, goalsB };
+}
+
+//calculo da tabela final
+export function calculateStandings(teams, matches) {
+    //estrutura da tabela
+  const table = teams.map(t => ({
+    name: t.nome,
+    points: 0,
+    goalDiff: 0
+  }));
+
+  //calculo de saldo e pontos com base nos gols de cada partida
+  matches.forEach(m => {
+    const a = table.find(t => t.name === m.teamA);
+    const b = table.find(t => t.name === m.teamB);
+
+    a.goalDiff += m.goalsA - m.goalsB;
+    b.goalDiff += m.goalsB - m.goalsA;
+
+    if (m.goalsA > m.goalsB) a.points += 3;
+    else if (m.goalsB > m.goalsA) b.points += 3;
+    else {
+      a.points += 1;
+      b.points += 1;
+    }
+  });
+
+  //ordenando a tabela seguindo os criterios estabelecidos
+  return table.sort((a, b) =>
+    b.points - a.points || b.goalDiff - a.goalDiff || Math.random() - 0.5
+  );
 }
